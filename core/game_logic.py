edeb2156335fb0190f3d16a75ba1e5d71d519e24
mendy hershkop <hers.mm@gmail.com>
2025-10-30 12:33:50 +0200

    fix bugs

--- core/game_logic.py
+++ core/game_logic.py
@@ -7,49 +7,57 @@ def calculate_hand_value(hand: list[dict]) -> int:
     elif hand['rank'] == "A":
         return 1
     return int(hand['rank'])
+
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     player['hand'] =calculate_hand_value(deck.pop())
     player['hand'] += calculate_hand_value(deck.pop())
     dealer['hand']=calculate_hand_value(deck.pop())
     dealer['hand'] += calculate_hand_value(deck.pop())
-    print("Player scour is ", player)
-    print("dealer scour is", dealer)
+    print("Player points is ", player)
+    print("dealer points is", dealer)
+
+
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
     while dealer['hand']<= 17:
         dealer['hand']+=calculate_hand_value(deck.pop())
     if dealer['hand'] >21:
         print("dealer score is", dealer['hand'],"dealer if falls, you win!!!")
         return False
-    print(dealer)
+    print("dealer have", dealer['hand'], "scour. let's chek who win")
     return True
+
+
 def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
     deal_two_each(deck,player,dealer)
-    player_scour=0
-    sre=0
     while True:
         game=ask_player_action()
         if game =="H":
-            player_hand=deck.pop()
-            print(player_hand)
-            player_scour+=calculate_hand_value(player_hand)
-            print((player_scour))
-            if player_scour >21:
+            player_hand=calculate_hand_value(deck.pop())
+            print("the cars is", player_hand)
+            print("The value of the card is", player_hand)
+            player['hand']+=player_hand
+            print(f"pleyer hes {player['hand']} points")
+            if player['hand'] >21:
                 print("Game Over...")
                 break
+            elif player['hand']==21:
+                print("you win with 21 points!!!")
+                break
             continue
+
         if game =="S":
             deal=dealer_play(deck,dealer)
             if not deal:
-                print(deal)
                 break
             if deal:
-                if player_scour >sre:
+                if player['hand'] >dealer['hand']:
                     print("player is won")
                     break
-                if player_scour<sre:
+                if player['hand']<dealer['hand']:
+                    print(f'You heve {player['hand']} points th dealer hes {dealer['hand']}. ')
                     print("dealer is won")
                     break
                 else:
-                    print("tie")
+                    print("You both have the same of points")
                     break
         break
