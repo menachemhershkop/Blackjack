ce7f8423c1c84abaaa64047949574254f67bf2f0
mendy hershkop <hers.mm@gmail.com>
2025-10-30 11:24:12 +0200

    create ask_player_action function

--- core/player_io.py
+++ core/player_io.py
@@ -1,4 +1,8 @@
 def ask_player_action() -> str:
-    pass
+    while True:
+        player_choice=input("Enter your choice. H for HIT. S for STAND: ")
+        answer= "s", "h", "S","H"
+        if player_choice in answer:
+            return player_choice.upper()
 def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
     pass
