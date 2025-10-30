18e9aac93657c218e3a0f8fcd2f5a3591301f4b3
mendy hershkop <hers.mm@gmail.com>
2025-10-30 11:19:18 +0200

    create ealer_play function

--- core/game_logic.py
+++ core/game_logic.py
@@ -6,7 +6,7 @@ def calculate_hand_value(hand: list[dict]) -> int:
     return int(hand['rank'])
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     player.update(card1=deck.pop(),card2=deck.pop())
-    dealer.update(card1=deck.pop(),card=deck.pop())
+    dealer.update(card1=deck.pop(),card2=deck.pop())
     player_scour=0
     dealer_scour=0
     print(len(player))
@@ -18,5 +18,11 @@ def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
     print("Player scour is ", player_scour)
     print("dealer scour is", dealer_scour)
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
-    pass
+    dealer_scour=0
+    while dealer_scour<= 17:
+        dealer_hand=deck.pop()
+        dealer_scour+=calculate_hand_value(dealer_hand)
+    if dealer_scour >21:
+        return True
+    return True
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from core.deck import build_standard_deck, shuffle_by_suit
-from core.game_logic import deal_two_each
+from core.game_logic import deal_two_each, dealer_play
 
 if __name__=='__main__':
-    deal_two_each(shuffle_by_suit(build_standard_deck()),{},{})
+    # deal_two_each(shuffle_by_suit(build_standard_deck()),{},{})
+    dealer_play(shuffle_by_suit(build_standard_deck()),{})
